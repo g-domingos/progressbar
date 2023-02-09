024fd8b1f10a7fdfd21f21fa04de6b7a330914fb
Gabriel <gdomingosss@gmail.com>
2023-02-09 20:50:23 -0300

    iudpate

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={window.location.pathname || ""}>
       <GlobalStyle />
       <Router />
     </BrowserRouter>
