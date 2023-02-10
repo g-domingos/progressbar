b2eddacfba846955c4cde156b5c15a3831d9f0e8
Gabriel <gdomingosss@gmail.com>
2023-02-09 22:49:36 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Router } from "./Router";
 import { GlobalStyle } from "./styles/global";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <BrowserRouter basename={window.location.pathname || ""}>
+    <HashRouter>
       <GlobalStyle />
       <Router />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
