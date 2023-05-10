49d6f3831b84720c951b31176aaccaf09ea4c514
Gabriel Domingos <Gabriel Domingos>
2023-05-09 22:53:25 -0300

    show details

--- src/pages/Clients/index.tsx
+++ src/pages/Clients/index.tsx
@@ -3,8 +3,11 @@ import { url } from "../../env";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
 import {
+  ButtonShowDetails,
   ColumnsDivs,
+  Details,
   MainDiv,
+  Responsible,
   Span,
   Task,
   TasksContainer,
@@ -12,6 +15,7 @@ import {
   Title,
   Tooltip,
 } from "./styles";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
 export const Clients = () => {
   const location = useLocation();
@@ -25,6 +29,7 @@ export const Clients = () => {
     id: "",
     show: true,
   });
+  const [showDetails, setShowDetails] = useState<any>({ id: "", status: "" });
 
   const getClientDetails = () => {
     setProcessing(true);
@@ -35,6 +40,14 @@ export const Clients = () => {
         setProcessing(false);
       })
       .catch((err: any) => console.log(err));
+  };
+
+  const handleExpand = (index: number, status: string) => {
+    if (showDetails.id === index && showDetails.status === status) {
+      setShowDetails({});
+    } else {
+      setShowDetails({ id: index, status: status });
+    }
   };
 
   useEffect(() => {
@@ -58,30 +71,64 @@ export const Clients = () => {
             {task?.subtasks
               .filter((sub: any) => sub.status.status !== "concluído")
               .map((item: any, index: number) => (
-                <Task>
-                  <div>
-                    <Span
-                      color={item.status.color}
-                      onMouseEnter={() =>
-                        setShowTooltipDone({ id: index, show: true })
-                      }
-                      onMouseLeave={() =>
-                        setShowTooltipDone({ id: "", show: false })
-                      }
+                <>
+                  <Task>
+                    <div>
+                      <Span
+                        color={item.status.color}
+                        onMouseEnter={() =>
+                          setShowTooltipDone({ id: index, show: true })
+                        }
+                        onMouseLeave={() =>
+                          setShowTooltipDone({ id: "", show: false })
+                        }
+                      >
+                        <div></div>
+                      </Span>
+                      <label>{item.name.toUpperCase()}</label>
+                    </div>
+                    {showTooltipDone?.id === index && showTooltipDone.show && (
+                      <Tooltip>{item.status.status.toUpperCase()}</Tooltip>
+                    )}
+                    <div>
+                      <label>
+                        {item.due_date
+                          ? new Intl.DateTimeFormat("pt-BR").format(
+                              item.due_date
+                            )
+                          : "-"}
+                      </label>
+                    </div>
+                    <ButtonShowDetails
+                      onClick={() => handleExpand(index, "unconcluded")}
                     >
-                      <div></div>
-                    </Span>
-                    <label>{item.name.toUpperCase()}</label>
-                  </div>
-                  {showTooltipDone?.id === index && showTooltipDone.show && (
-                    <Tooltip>{item.status.status.toUpperCase()}</Tooltip>
-                  )}
-                  <div>
-                    <label>
-                      {new Intl.DateTimeFormat("pt-BR").format(item.due_date)}
-                    </label>
-                  </div>
-                </Task>
+                      {showDetails.id !== index ? (
+                        <IoIosArrowDown />
+                      ) : (
+                        <IoIosArrowUp />
+                      )}
+                    </ButtonShowDetails>
+                  </Task>
+                  {showDetails.id === index &&
+                    showDetails.status === "unconcluded" && (
+                      <Details>
+                        <div>
+                          <label>DETALHES</label>
+                          <label>
+                            {item.assignees.length > 1
+                              ? "RESPONSÁVEIS"
+                              : "RESPONSÁVEL"}
+                          </label>
+                        </div>
+                        {item.assignees.map((resp: any) => (
+                          <Responsible>
+                            <img src={resp.profilePicture} />
+                            <label>{resp.username}</label>
+                          </Responsible>
+                        ))}
+                      </Details>
+                    )}
+                </>
               ))}
           </TasksContainer>
         </div>
@@ -97,33 +144,66 @@ export const Clients = () => {
               .sort((a: any, b: any) => b.date_updated - a.date_updated)
               .filter((sub: any) => sub.status.status === "concluído")
               .map((item: any, index: number) => (
-                <Task>
-                  <div>
-                    <Span
-                      color={item.status.color}
-                      onMouseEnter={() =>
-                        setShowTooltip({ id: index, show: true })
-                      }
-                      onMouseLeave={() =>
-                        setShowTooltip({ id: "", show: false })
-                      }
+                <>
+                  <Task
+                  // onMouseEnter={() => setShowDetails(index)}
+                  // onMouseLeave={() => setShowDetails("")}
+                  >
+                    <div>
+                      <Span
+                        color={item.status.color}
+                        onMouseEnter={() =>
+                          setShowTooltip({ id: index, show: true })
+                        }
+                        onMouseLeave={() =>
+                          setShowTooltip({ id: "", show: false })
+                        }
+                      >
+                        <div></div>
+                      </Span>
+                      {/* <img src={item.creator.profilePicture} /> */}
+                      <label>{item.name.toUpperCase()}</label>
+                    </div>
+                    <div>
+                      <label>
+                        {new Intl.DateTimeFormat("pt-BR").format(
+                          item.date_updated
+                        )}
+                      </label>
+                    </div>
+                    {showTooltip?.id === index && showTooltip.show && (
+                      <Tooltip>{item.status.status.toUpperCase()}</Tooltip>
+                    )}
+                    <ButtonShowDetails
+                      onClick={() => handleExpand(index, "concluded")}
                     >
-                      <div></div>
-                    </Span>
-                    {/* <img src={item.creator.profilePicture} /> */}
-                    <label>{item.name.toUpperCase()}</label>
-                  </div>
-                  <div>
-                    <label>
-                      {new Intl.DateTimeFormat("pt-BR").format(
-                        item.date_updated
+                      {showDetails.id !== index ? (
+                        <IoIosArrowDown />
+                      ) : (
+                        <IoIosArrowUp />
                       )}
-                    </label>
-                  </div>
-                  {showTooltip?.id === index && showTooltip.show && (
-                    <Tooltip>{item.status.status.toUpperCase()}</Tooltip>
-                  )}
-                </Task>
+                    </ButtonShowDetails>
+                  </Task>
+                  {showDetails.id === index &&
+                    showDetails.status === "concluded" && (
+                      <Details>
+                        <div>
+                          <label>DETALHES</label>
+                          <label>
+                            {item.assignees.length > 1
+                              ? "RESPONSÁVEIS"
+                              : "RESPONSÁVEL"}
+                          </label>
+                        </div>
+                        {item.assignees.map((resp: any) => (
+                          <Responsible>
+                            <img src={resp.profilePicture} />
+                            <label>{resp.username}</label>
+                          </Responsible>
+                        ))}
+                      </Details>
+                    )}
+                </>
               ))}
           </TasksContainer>
         </div>
--- src/pages/Clients/styles.ts
+++ src/pages/Clients/styles.ts
@@ -37,10 +37,11 @@ export const ColumnsDivs = styled.div`
   display: flex;
   flex-direction: row;
   margin-bottom: 30px;
+  justify-content: space-between;
 
   & > div {
     padding: 10px;
-    width: 50%;
+    width: 48%;
   }
 
   @media screen and (max-width: 767px) {
@@ -136,4 +137,87 @@ export const Tooltip = styled.div`
   font-size: 11px;
   padding: 7px;
   border-radius: 4px;
+`;
+
+export const Details = styled.div`
+  padding: 5px;
+  border-radius: 4px;
+  background: #f1f1f1;
+  /* animation: scale-up-ver-top 0.4s cubic-bezier(0.39, 0.575, 0.565, 1) both; */
+  animation: slide-bottom 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
+
+  & > div:first-child {
+    display: flex;
+    flex-direction: column;
+    font-size: 12px;
+
+    & > label {
+      margin-bottom: 10px;
+    }
+    & > label:first-child {
+      font-weight: 700;
+      color: #807d7d;
+    }
+    & > label:last-child {
+      font-weight: 500;
+      color: #807d7d;
+      font-size: 10px;
+    }
+  }
+
+  @keyframes scale-up-ver-top {
+    0% {
+      -webkit-transform: scaleY(0.4);
+      transform: scaleY(0.4);
+      -webkit-transform-origin: 100% 0%;
+      transform-origin: 100% 0%;
+    }
+    100% {
+      -webkit-transform: scaleY(1);
+      transform: scaleY(1);
+      -webkit-transform-origin: 100% 0%;
+      transform-origin: 100% 0%;
+    }
+  }
+
+  @keyframes slide-bottom {
+    0% {
+      transform: translateY(-10px);
+      opacity: 0;
+    }
+    100% {
+      transform: translateY(0px);
+      opacity: 1;
+    }
+  }
+`;
+
+export const Responsible = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.4rem;
+  margin-bottom: 5px;
+  font-weight: 500;
+  font-size: 10px;
+
+  img {
+    width: 2rem;
+    height: 2rem;
+    border-radius: 100%;
+  }
+`;
+
+export const ButtonShowDetails = styled.button`
+  position: absolute;
+  transform: scale(1);
+  right: -1.2rem;
+  background: none;
+  border: none;
+  color: #807d7d;
+  transition: color 0.3s ease-in-out transform 0.3s ease-in-out;
+
+  :hover {
+    color: black;
+    transform: scale(1.2);
+  }
 `;
